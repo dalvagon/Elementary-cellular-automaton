68687890f6d95f4f1be5783628522b7b88281946
Leahu Vlad Iulius <leahu.vlad.iulius@gmail.com>
2023-03-15 15:01:14 +0200

    Generate README

---
+++ generateReadMe.py
@@ -0,0 +1,9 @@
+def generate():
+    with open("README.md", "w") as f:
+        for rule in range(0, 256):
+            s = f"Rule {rule}: \ \n ![alt text](https://github.com/dalvagon/Elementary-cellular-automaton/blob/main/assets/rule{rule}.png?raw=true) \n"
+            f.write(s)
+
+
+if __name__ == "__main__":
+    generate()
